fa8dd42b0409254c406545c0af9280a1e5caa570
zishanx <zishanalikhan53@gmail.com>
2026-07-20 09:43:34 +0530

    Socket io connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
-const io = new Server(server);
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+
+const server = http.createServer(app)
+
+const io = new Server(server)
 
 io.on("connection", (socket) => {
-    console.log("A user connected")
+    console.log("user connected");
+    console.log(socket.id);
+})
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
